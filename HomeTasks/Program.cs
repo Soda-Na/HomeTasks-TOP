976050633d3ea3393d7928e3931a9f04ae330592
Soda <mathematicvlad@gmail.com>
2024-01-30 20:30:18 +0300

    ь

--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace HomeTasks
 {
+    public List<int> PrefixFunc(string s)
+
+    {
+        List<int> p = new List<int>(s.Length);
+        p.Add(0);
+        for (int i = 1; i < s.Length; i++)
+        {
+            int k = p[i - 1];
+            while (k > 0 && s[i] != s[k])
+                k = p[k - 1];
+            if (s[i] == s[k])
+                k++;
+            p.Add(k);
+        }
+        return p;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
