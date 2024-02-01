770474578c0f05fc735f82f6330945827d49cde3
Soda <mathematicvlad@gmail.com>
2024-02-01 16:13:59 +0300

    jj

---
+++ HomeTasks/Figures.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTasks
+{
+    abstract class Figure
+    {
+        public abstract double Area();
+    }
+
+    class Rectangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+
+    class RightTriangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public RightTriangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height / 2;
+        }
+    }
+
+    class Trapezium : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Base { get; set; }
+
+        public Trapezium(double width, double height, double _base)
+        {
+            Width = width;
+            Height = height;
+            Base = _base;
+        }
+
+        public override double Area()
+        {
+            return (Base + Width) * Height / 2;
+        }
+    }
+
+    class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    internal class Figures
+    {
+        public void Main()
+        {
+            Rectangle rectangle = new Rectangle(10, 20);
+            RightTriangle rightTriangle = new RightTriangle(10, 20);
+            Trapezium trapezium = new Trapezium(10, 20, 30);
+            Circle circle = new Circle(10);
+
+            Console.WriteLine($"Rectangle area: {rectangle.Area()}");
+            Console.WriteLine($"RightTriangle area: {rightTriangle.Area()}");
+            Console.WriteLine($"Trapezium area: {trapezium.Area()}");
+            Console.WriteLine($"Circle area: {circle.Area()}");
+        }
+    }
+}
--- HomeTasks/Program.cs
+++ HomeTasks/Program.cs
@@ -1,15 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeTasks
 {
+
+    class Device
+    {
+        public virtual string Sound { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+        public Device(string sound, string name, string description)
+        {
+            Sound = sound;
+            Name = name;
+            Description = description;
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine(Sound);
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nDescription: {Description}");
+        }
+    }
+
+    class Kettle : Device
+    {
+        public Kettle(
+            string sound = "bzz", 
+            string name = "Kettle", 
+            string description = "This is a kettle"
+            ) : base(sound, name, description)
+        {
+        }
+    }
+
+    class Microwave : Device
+    {
+        public Microwave(
+            string sound = "bzz", 
+            string name = "Microwave", 
+            string description = "This is a microwave"
+            ) : base(sound, name, description)
+        {
+        }
+    }
+
+    class Auto : Device
+    {
+        public Auto(
+            string sound = "bip", 
+            string name = "Auto", 
+            string description = "This is a auto"
+            ) : base(sound, name, description)
+        {
+        }
+    }
+
+    class Streamboat : Device
+    {
+        public Streamboat(
+            string sound = "bip", 
+            string name = "Streamboat", 
+            string description = "This is a stremaboat"
+            ) : base(sound, name, description)
+        {
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Kettle kettle = new Kettle();
+            kettle.MakeSound();
+            kettle.ShowInfo();
+
+            Microwave microwave = new Microwave();
+            microwave.MakeSound();
+            microwave.ShowInfo();
+
+            Auto auto = new Auto();
+            auto.MakeSound();
+            auto.ShowInfo();
+
+            Streamboat streamboat = new Streamboat();
+            streamboat.MakeSound();
+            streamboat.ShowInfo();
         }
     }
 }
